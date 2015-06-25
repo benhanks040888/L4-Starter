ccb18a9f18087eeedfd3523b4f307561a79b92da
Hendra Susanto <benhanks040888@gmail.com>
2015-06-25 11:48:32 +0700

    move admin routes to separate file

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+include('admin_routes.php');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,24 +15,4 @@
 
 Route::group(array('namespace' => 'App\Controllers'), function() {
   Route::get('/', array('as' => 'home', 'uses' => 'HomeController@getIndex'));
-});
-
-
-/*
- * 
- * Admin Route
- */
-Route::group(array('prefix' => 'admin', 'namespace' => 'App\Controllers\Admin'), function() {
-  Route::get('/', array('as' => 'admin.login', 'uses' => 'AuthController@getIndex'));
-  Route::get('logout', array('as' => 'admin.logout', 'uses' => 'AuthController@getLogout'));
-
-  Route::post('/', array('as' => 'admin.login.submit', 'uses' => 'AuthController@postLogin'));
-
-  // These routes is only accessible when admin is already logged in
-  Route::group(array('before' => 'admin'), function() {
-    Route::get('dashboard', array('as' => 'admin.dashboard', 'uses' => 'SiteController@getIndex'));
-
-    Route::get('change-password', array('as' => 'admin.change-password', 'uses' => 'AuthController@getChangePassword'));
-    Route::post('change-password', array('as' => 'admin.change-password.submit', 'uses' => 'AuthController@postChangePassword'));
-  });
 });
