38a19dbece2e1c8a5c4a0dfa221e985708bb6db9
Hendra Susanto <benhanks040888@gmail.com>
2016-05-17 00:53:41 +0700

    add og:type of website

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -10,15 +10,17 @@
     <meta name="description" content="@yield('description', Config::get('site.description'))">
 
     @section('twitter_meta')
-<meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary" />
+    <!-- Twitter handle of the site -->
     <meta name="twitter:site" content="" />
     @show
 
     @section('og')
-<meta property="fb:app_id" content="{{ Config::get('facebook.appId') }}" />
+    <meta property="fb:app_id" content="{{ Config::get('facebook.appId') }}" />
 
     <meta property="og:site_name" content="{{ Config::get('site.name') }}" /> 
     <meta property="og:url" content="{{ URL::current() }}" /> 
+    <meta property="og:type" content="website" /> 
     <meta property="og:title" content="@yield('og:title', Config::get('site.title'))" /> 
     <meta property="og:description" content="@yield('description', Config::get('site.description'))" /> 
     <meta property="og:image" content="@yield('og:image', Config::get('site.default_image') )" />
@@ -35,9 +37,9 @@
     <link rel="apple-touch-icon" sizes="76x76" href="{{ assets_url('images/favicons/apple-touch-icon-76x76.png') }}">
     <link rel="apple-touch-icon" sizes="114x114" href="{{ assets_url('images/favicons/apple-touch-icon-114x114.png') }}">
     <link rel="apple-touch-icon" sizes="120x120" href="{{ assets_url('images/favicons/apple-touch-icon-120x120.png') }}">
+    <link rel="icon" type="image/png" href="{{ assets_url('images/favicons/favicon-16x16.png') }}" sizes="16x16">
     <link rel="icon" type="image/png" href="{{ assets_url('images/favicons/favicon-32x32.png') }}" sizes="32x32">
     <link rel="icon" type="image/png" href="{{ assets_url('images/favicons/favicon-96x96.png') }}" sizes="96x96">
-    <link rel="icon" type="image/png" href="{{ assets_url('images/favicons/favicon-16x16.png') }}" sizes="16x16">
 
     <link rel="stylesheet" href="{{ assets_url('css/vendors/bootstrap/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ assets_url('css/site.prefixed.min.css') }}?v={{ filemtime(public_path() . '/assets/css/site.prefixed.min.css') }}">
